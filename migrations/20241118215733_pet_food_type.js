8062f86c5fe28393ae742f65d5d864fd56ad58f8
roc155950@gmail.com <roc155950@gmail.com>
2024-11-18 21:04:48 -0500

    FinalKnexPush

---
+++ migrations/20241118215733_pet_food_type.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('pet_food_type', table => {
+        table.increments();
+        table.string('name', 100);
+        table.string('description', 100);
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('food_type');
+};
--- seeds/pet_type.js
+++ seeds/pet_type.js
@@ -2,12 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
-exports.seed = async function(knex) {
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  return knex('pet_type').del()
+    .then(() => {
+      // Inserts seed entries
+      return knex('pet_type').insert([
+        { id: 1, name: 'Bird' },
+        { id: 2, name: 'Cat' },
+        { id: 3, name: 'Dog' },
+        { id: 4, name: 'Fish' },
+      ]);
+    });
 };
+
